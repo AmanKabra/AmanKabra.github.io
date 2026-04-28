c3562af838002306b2ee439234e5cefd12d0c289
AmanKabra <49343582+AmanKabra@users.noreply.github.com>
2026-04-28 20:46:39 +0000

    Deploying to gh-pages from @ AmanKabra/AmanKabra.github.io@1f1002bc2c6332cc5ba8a12718996324a29c87b7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,22 +44,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/contact/";
           },
-        },{id: "news-joined-a-2-2m-u-s-army-research-institute-grant-on-task-interdependence-in-multiteam-systems-as-the-doctoral-student-lead-at-penn-state-pi-hollenbeck-humphrey-lepine",
-          title: 'Joined a $2.2M U.S. Army Research Institute grant on task interdependence in multiteam...',
-          description: "",
-          section: "News",},{id: "news-received-the-ossian-r-mackenzie-teaching-award-from-penn-state-awarded-annually-to-one-doctoral-student-across-all-departments-in-the-smeal-college-of-business-for-teaching-performance",
-          title: 'Received the Ossian R. MacKenzie Teaching Award from Penn State, awarded annually to...',
-          description: "",
-          section: "News",},{id: "news-presented-a-symposium-on-architecting-empirical-frontiers-national-infrastructure-to-investigate-multiteam-systems-at-the-84th-annual-meeting-of-the-academy-of-management-in-chicago",
-          title: 'Presented a symposium on Architecting Empirical Frontiers: National Infrastructure to Investigate Multiteam Systems...',
-          description: "",
-          section: "News",},{id: "news-our-paper-why-and-how-organizational-structures-change-has-been-published-in-the-academy-of-management-annals-with-m-m-luciano-m-k-watson-and-c-c-winchester",
-          title: 'Our paper “Why and How Organizational Structures Change” has been published in the...',
-          description: "",
-          section: "News",},{id: "news-i-will-be-joining-the-department-of-management-at-the-spears-school-of-business-oklahoma-state-university-as-a-tenure-track-assistant-professor-in-organizational-behavior-effective-july-2026",
-          title: 'I will be joining the Department of Management at the Spears School of...',
-          description: "",
-          section: "News",},{id: "teachings-entrepreneurial-mindset-mgmt-215",
+        },{id: "teachings-entrepreneurial-mindset-mgmt-215",
           title: 'Entrepreneurial Mindset (MGMT 215)',
           description: "A 3-credit undergraduate course on entrepreneurial reasoning, customer discovery, and problem-based learning.",
           section: "Teachings",handler: () => {
