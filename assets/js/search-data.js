178477f7a14bbc7fdf91771f5721000e7e73b71e
AmanKabra <49343582+AmanKabra@users.noreply.github.com>
2026-05-25 20:37:44 +0000

    Deploying to gh-pages from @ AmanKabra/AmanKabra.github.io@3f3b89b99092d3641d8e4a3113a1cbaf7ef872b7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -61,7 +61,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%61%6D%61%6E.%6B%61%62%72%61@%70%73%75.%65%64%75", "_blank");
+          window.open("mailto:%61%6D%61%6E%6B%61%62%72%61%34@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-linkedin',
